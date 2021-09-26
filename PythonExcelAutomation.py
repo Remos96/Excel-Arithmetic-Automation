6c4e444562e0fe2ac012e7b911ce71ab9ff0840a
Remos96 <80366356+Remos96@users.noreply.github.com>
2021-09-26 15:49:11 -0400

    Update PythonExcelAutomation.py

--- PythonExcelAutomation.py
+++ PythonExcelAutomation.py
@@ -13,11 +13,13 @@ def update_workbook(file_name):
     }
 
 
-    wrkbk = pyxl.load_workbook(file_name)                                                                                       # Load the user specified excel workbook
-    sheet_selected = input("Which sheet are you accessing? Type it exactly, with no spaces, between 'Sheet' and the number: ")
-    sheet = wrkbk[sheet_selected]                                                                                               # Retrieve the user specified desired excel sheet
+    wrkbk = pyxl.load_workbook(file_name)   # Load the user specified excel workbook.
+    user_sheet = input("Which sheet number are you accessing: ")
+    user_sheet_formatted = "Sheet" + user_sheet
+    sheet = wrkbk[user_sheet_formatted]     # Retrieve the user specified desired excel sheet.
     print("Your selected sheet has", sheet.max_row, "rows.")
-    iterated_rows = int(input("How many rows would you like to iterate for: "))
+    start_row = int(input("Which row would you like to start from? Make sure to skip title/header rows: "))
+    end_row = int(input("Which row would you like to end on: "))
     user_operation = input("What operation are we performing? Please type multiplying, dividing, adding, or subtracting: ").lower()
 
     while (user_operation not in ["multiplying", "dividing", "adding", "subtracting"]):
@@ -28,21 +30,30 @@ def update_workbook(file_name):
     col_selected = int(input(f"Which column number values should the program be {user_operation} {operation_value} by (this column should contain your initial/original values): "))
     new_value_col = int(input("Which column number should the program store the newly calculated values: "))
 
-    for row in range(2, iterated_rows + 1):                                                 # Start from 2 in order to ignore named/titled first row
+    for row in range(start_row, end_row + 1):
         cell = sheet.cell(row, col_selected)
-        corrected_price = operations[f"{user_operation}"](cell.value, operation_value)      # Allows the earlier, user specified, operation to be used here to calc new price
-        corrected_price_cell = sheet.cell(row, new_value_col)                               # Creates the cell object
-        corrected_price_cell.value = corrected_price                                        # Value of cell object now holds corrected price
+        try:
+            corrected_price = operations[f"{user_operation}"](cell.value, operation_value)      # Allows the earlier, user specified, operation to be used here to calculate new price.
+        except TypeError:
+            corrected_price = ""
+        corrected_price_cell = sheet.cell(row, new_value_col)   # Creates cell object that will hold newly calculated value.
+        corrected_price_cell.value = corrected_price            # Value of said cell object now holds corrected price.
 
-    make_graph = input("Would you like to a basic bar graph of the newly calculated values to be made? Please type 'Yes' or 'No': ").lower()
+
+    print("""Would you like to a basic bar graph of the newly calculated values to be made? 
+    *Note: If in the range of selected rows any of the selected rows had empty cells, the bar graph will also graph those empty cells resulting in a blank column for that row.*""")
+    make_graph = input("Please type Yes or 'No': ").lower()
     if make_graph == "yes":
         graph_cell = input("In what cell would you like to place the graph in: ")
-        values = Reference(sheet, min_row = 2, max_row = sheet.max_row, min_col = new_value_col, max_col = new_value_col)   # Reference, to select a range of values - setup our future graph
+        values = Reference(sheet, min_row = start_row, max_row = end_row, min_col = new_value_col, max_col = new_value_col)     # Reference, to select a range of values - setup our potential future graph.
         chart = BarChart()
         chart.add_data(values)
-        sheet.add_chart(chart, f"{graph_cell}")     # Place chart starting in user-defined cell
-    elif make_graph == "no":
-        pass
+        sheet.add_chart(chart, f"{graph_cell}")     # Place chart starting in user-defined cell.
 
-    new_file_name = input("What would you like to save the file as: ")
-    wrkbk.save(new_file_name)                                                                                                       
+
+    make_new_file = input("Would you like to save as a new file? Please type yes or no: ").lower()
+    if make_new_file == "yes":
+        new_file_name = input("What would you like to save the file as: ")
+        wrkbk.save(new_file_name)
+    else:
+        wrkbk.save(file_name)
